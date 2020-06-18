eb90b996137345eedfed1eb287f0126f93be169b
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-06-18 00:45:05 -0700

    enhancing folder completed

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,30 @@
 module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
+	succeed,
+	fail,
+	repair,
 };
 
 function succeed(item) {
-  return { ...item };
+	return {
+		...item,
+		enhancement:
+			item.enhancement !== 20 ? item.enhancement + 1 : item.enhancement,
+	};
 }
 
 function fail(item) {
-  return { ...item };
+	return {
+		...item,
+		enhancement:
+			item.enhancement !== 20 ? item.enhancement - 1 : item.enhancement,
+		durability:
+			item.enhancement <= 15 ? item.durability - 5 : item.durability - 10,
+	};
 }
 
 function repair(item) {
-  return { ...item };
-}
-
-function get(item) {
-  return { ...item };
+	return {
+		...item,
+		durability: 100,
+	};
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,48 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe(' Enchanting', () => {
+	describe('repair()', () => {
+		it('should return the item durability repair to 100', () => {
+			const axe = {
+				durability: 26,
+				enhancement: 1,
+			};
+			expect(enhancer.repair(axe).durability).toBe(100);
+		});
+	});
+
+	describe('succeed', () => {
+		const Staff = {
+
+			durability: 66,
+			enhancement: 11,
+    };
+         const wand = {
+
+			durability: 12,
+			enhancement: 20,
+        };
+        
+	it('should return item enhancement + 1', () => {
+			expect(enhancer.succeed(Staff).enhancement).toBe(12);
+    });
+    it('should not enchant a item at 20 already', () => {
+			expect(enhancer.succeed(wand).enhancement).toBe(20);
+		});
+  });
+  
+  describe('fail', () => {
+		const Bow = {
+			durability: 76,
+			enhancement: 11,
+        };
+        
+	test('should return item enhancement - 1', () => {
+			expect(enhancer.fail(Bow).enhancement).toBe(10);
+    });
+    test('should return durabiulity - 10', () => {
+			expect(enhancer.fail(Bow).durability).toBe(71);
+		});
+  });
+});
